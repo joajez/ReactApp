04da3e66a2fceca214c36799d0690a7e3aa324f9
Joanna Jeziorek <asia.jeziorekk@gmail.com>
2020-06-03 23:17:17 +0200

    move router to app

--- fiszki/src/App.js
+++ fiszki/src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Menu from './Components/Menu/Menu';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route  } from "react-router-dom";
 
 import './App.css';
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Create from './Components/Create/Create'
+import Learn from './Components/Learn/Learn'
+import Ideas from './Components/Ideas/Ideas'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Menu />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Menu />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/create">
+                  <Create />
+                </Route>
+                <Route path="/learn">
+                  <Learn />
+                </Route>
+                <Route path="/ideas">
+                  <Ideas />
+                </Route>
+              </Switch>
+      </div>
+    </Router>
+
   );
 
 }
--- fiszki/src/Components/Menu/Menu.js
+++ fiszki/src/Components/Menu/Menu.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    NavLink,
-    Route  } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import './Menu.css';
-import Home from '../Home/Home'
-import About from '../About/About'
-import Create from '../Create/Create'
-import Learn from '../Learn/Learn'
-import Ideas from '../Ideas/Ideas'
+
 
 import menuComponents from '../../config/menuConfig';
 
@@ -23,25 +15,14 @@ const Menu = () => {
     }
 
     return (
-        <Router>
             <div>
                 <nav className='menu'>
                     <ul className='menu-list'>
                         {menuItems}
                     </ul>
                 </nav>
-            <section>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/create" component={Create}/>
-                    <Route path="/learn" component={Learn}/>
-                    <Route path="/ideas" component={Ideas}/>
-                </Switch>
-            </section>
             </div>
-        </Router>
         )
     }
 
-    export default Menu;
+export default Menu;
